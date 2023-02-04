a9d02e9fd12acfa23af96f1d6c046c058195a938
PatelKurvesh <patelkurvesh1999@gmail.com>
2023-02-04 16:59:19 +0530

    View and model

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,6 +4,12 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("GithubSAP-UI5-Fiori-14_Navigation.controller.View1", {
+		
+		onInit : function(){
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("model/fruits.json");
+			this.getView().setModel(oModel);
+		}
 
 	});
 });
